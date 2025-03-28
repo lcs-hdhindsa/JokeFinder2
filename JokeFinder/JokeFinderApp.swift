3bbfe1c81eea38199fb2e3b5075ec882644afe2f
Daniel Li <danli@lcs.on.ca>
2025-03-28 12:16:42 -0400

    Initial Commit

---
+++ JokeFinder/JokeFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  JokeFinderApp.swift
+//  JokeFinder
+//
+//  Created by 李泽宇 on 2025-03-28.
+//
+
+import SwiftUI
+
+@main
+struct JokeFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
